18981aa5846c5935d06f80c9a4a9aeb89aa370c5
dayyan-269 <101689434+dayyan-269@users.noreply.github.com>
2024-11-15 16:09:59 +0000

    Deploying to gh-page from @ dayyan-269/dayyan-269.github.io@ba02136d5d5bca971dd9c531f326b1d46d4a426a 🚀

---
+++ _next/static/chunks/app/page-e2cef8f461425b9c.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{204:function(e,t,r){Promise.resolve().then(r.bind(r,6714))},6714:function(e,t,r){"use strict";r.r(t);var n=r(9268);let o=e=>{let{children:t,className:r,compType:o="button",onClickHandler:c}=e;return(0,n.jsx)("button",{className:"rounded p-2 bg-accent-color text-white ".concat(r),type:o,onClick:c,children:t})};t.default=o},3177:function(e,t,r){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=r(6006),o=Symbol.for("react.element"),c=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,s=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};function i(e,t,r){var n,c={},i=null,l=null;for(n in void 0!==r&&(i=""+r),void 0!==t.key&&(i=""+t.key),void 0!==t.ref&&(l=t.ref),t)u.call(t,n)&&!f.hasOwnProperty(n)&&(c[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===c[n]&&(c[n]=t[n]);return{$$typeof:o,type:e,key:i,ref:l,props:c,_owner:s.current}}t.Fragment=c,t.jsx=i,t.jsxs=i},9268:function(e,t,r){"use strict";e.exports=r(3177)}},function(e){e.O(0,[667,139,744],function(){return e(e.s=204)}),_N_E=e.O()}]);
